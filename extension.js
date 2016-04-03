07ad2bc0ea57d1646b8e52e632b7a6a8dd4011db
jeffa <jeffa00@gmail.com>
2016-04-03 09:15:54 -0700

    Added base for insert file link.

--- extension.js
+++ extension.js
@@ -4,6 +4,23 @@
 // Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
 
+let insertText = (value) => {
+  let editor = vscode.window.activeTextEditor;
+
+  if(!editor) {
+      vscode.window.showErrorMessage("Can't insert text because no document is open.");
+      return;
+  }
+
+  let selection = editor.selection;
+
+  let range = new vscode.Range(selection.start, selection.end);
+
+  editor.edit((editBuilder) =>{
+     editBuilder.replace(range, value);
+  });
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -12,20 +29,13 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "static-site-hero" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let helloDisposable = vscode.commands.registerCommand('extension.sayHello', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(helloDisposable);
-
     let fileLinkDisposable = vscode.commands.registerCommand('extension.insertLink', () => {
-        vscode.window.showInformationMessage('Insert File Link Initiated.');
+        let linkTypeList = ['File', 'Image'];
+
+        vscode.window.showQuickPick(linkTypeList, {placeHolder: 'Link Type'})
+            .then(result => {
+               insertText(result);
+            });
     });
 
     context.subscriptions.push(fileLinkDisposable);
