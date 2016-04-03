c2b0e820344e70318e39739d5658d4803e0927c2
jeffa <jeffa00@gmail.com>
2016-04-02 21:41:06 -0700

    Added functions and tracer bullets

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
+'use strict';
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+let vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,19 +10,32 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "static-site-hero" is now active!'); 
+	console.log('Congratulations, your extension "static-site-hero" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+	let helloDisposable = vscode.commands.registerCommand('extension.sayHello', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	
-	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(helloDisposable);
+
+    let fileLinkDisposable = vscode.commands.registerCommand('extension.insertLink', () => {
+        vscode.window.showInformationMessage('Insert File Link Initiated.');
+    });
+
+    context.subscriptions.push(fileLinkDisposable);
+
+    let figureDisposable = vscode.commands.registerCommand('extension.insertFigure', () => {
+        vscode.window.showInformationMessage('Insert Figure Tag Initiated');
+    });
+
+    context.subscriptions.push(figureDisposable);
+
 }
 exports.activate = activate;
 
